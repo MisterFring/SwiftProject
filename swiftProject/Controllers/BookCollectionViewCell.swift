6737b83bb40440ce75aa040d611051c39d353357
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-02-24 20:59:56 +0100

    favoris

---
+++ swiftProject/Controllers/BookCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookCollectionViewCell.swift
+//  swiftProject
+//
+//  Created by Magali Dugué on 23/02/2022.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    
+}
