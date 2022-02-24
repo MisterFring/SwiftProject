02bc715b1962d9ea256914581b77300427408950
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2022-02-24 23:00:25 +0100

    navigation

--- swiftProject/Controllers/HomeController.swift
+++ swiftProject/Controllers/HomeController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
+
+
 class HomeController: UIViewController {
+    
+  
 
     @IBOutlet weak var coverImageView: UIImageView!
     
+        
+        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(sender:)))
+               tapGestureRecognizer.numberOfTapsRequired = 2
+               self.coverImageView.isUserInteractionEnabled = true
+               self.coverImageView.addGestureRecognizer(tapGestureRecognizer)
+        
         
         ApiManager.shared.fetchBook(bookId : "OL45883W", { book in
             print(book)
@@ -26,5 +40,11 @@ class HomeController: UIViewController {
     
     @IBAction func likeButton(_ sender: Any) {
     }
+    @objc func didTap(sender : UITapGestureRecognizer){
+        print("on est passé ici")
+         if let vc = UIStoryboard(name:"Main",bundle:nil).instantiateViewController(identifier:"Details") as? DetailsViewController{
+             self.navigationController?.pushViewController(vc, animated: true)
+            
+        }
 }
-
+}
